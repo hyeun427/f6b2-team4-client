7059a2efaf2f6dfd2e0c7ea7e4296c8bb5b0e4fe
hyeun427 <80902406+hyeun427@users.noreply.github.com>
2022-05-21 16:48:26 +0900

    Delete src directory

--- src/commons/libraries/utils.ts
+++
@@ -1,8 +0,0 @@
-export const getDate = (date: string | number | Date) => {
-  const newDate = new Date(date);
-  const yyyy = String(newDate.getFullYear()).padStart(4, '0');
-  const mm = String(newDate.getMonth() + 1).padStart(2, '0');
-  const dd = String(newDate.getDate()).padStart(2, '0');
-  const yyyymmdd: String = `${yyyy}-${mm}-${dd}`;
-  return yyyymmdd;
-};
