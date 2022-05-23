3e501b54a334583a49f0881fb44eca37eac8e365
hyeun427 <hyeun427@gmail.com>
2022-05-23 16:21:41 +0900

    feature-#102

--- f6b2-client/src/components/units/gardenDetail/GardenDetail.presenter.tsx
+++ f6b2-client/src/components/units/gardenDetail/GardenDetail.presenter.tsx
@@ -1,75 +1,76 @@
 import * as S from "./GardenDetail.styles";
 import { getDate } from "../../../commons/libraries/utils";
 import { MdQuestionAnswer, MdThumbUp, MdBookmarkBorder } from "react-icons/md";
+import { useRecoilState } from "recoil";
+import { userInfoState } from "../../../commons/store";
+import GardenDetailCommentList from "./detailcomment/list/GardenCommentList.container";
 
 export default function GardenDetailUI(props: any) {
   console.log(props.data?.fetchBoard.commentsCount, "댓글수나오냐");
+  const [loginInfo] = useRecoilState(userInfoState);
   return (
     <>
       <S.GardenWrapper>
         <S.Wrapper>
           <S.GardenListBox key={props.data?.fetchBoard.id}>
-            <S.WriterInfoBox>
-              <S.WriterProfile />
-              <S.WriterInfo>
-                <S.NameRow>
+            <S.ContentsImg />
+
+            <S.RightWrapper>
+              <S.WriterInfoBox>
+                <S.WriterInfo>
+                  <S.WriterProfile />
                   <S.WriterName>
                     {props.data?.fetchBoard.writer.name}
                   </S.WriterName>
-                </S.NameRow>
-                <S.CreatedAt>
-                  {getDate(props.data?.fetchBoard.createdAt)}
-                </S.CreatedAt>
-              </S.WriterInfo>
-              {/* 수정버튼 */}
-              <S.IconWrapper>
-                <S.EditIcon />
-              </S.IconWrapper>
-            </S.WriterInfoBox>
-            <S.ContentsBox>
-              <S.Contents>{props.data?.fetchBoard.content}</S.Contents>
-              {/* 번역API 버튼 자리? */}
-              <S.ContentsTranslateBox>
-                <S.ContentsTranslate>번역한 내용</S.ContentsTranslate>
-              </S.ContentsTranslateBox>
-              <S.ContentsImg />
-
-              {/* 댓글 부분 다시 수정할 것 */}
-              <S.LikeAndCommentCountBox>
-                {props.commentListVal ? (
-                  <S.CommentListBtn
-                    onClick={props.onClickCommentListBtn}
-                    id={props.data?.fetchBoard.id}
-                  >
-                    close
-                  </S.CommentListBtn>
-                ) : (
-                  <S.CommentListBtn
-                    onClick={props.onClickCommentListBtn}
-                    id={props.data?.fetchBoard.id}
-                  >
-                    open
-                  </S.CommentListBtn>
+                  <S.CreatedAt>
+                    {getDate(props.data?.fetchBoard.createdAt)}
+                  </S.CreatedAt>
+                </S.WriterInfo>
+                {/* 수정버튼 */}
+                {props.data?.fetchBoard.writer.id === loginInfo?.id && (
+                  <S.IconWrapper>
+                    <S.EditIcon />
+                  </S.IconWrapper>
                 )}
+                {props.data?.fetchBoard.writer.id !== loginInfo?.id && ""}
+              </S.WriterInfoBox>
 
-                <S.LikeAndCommentCount>
-                  <S.Like>
-                    <MdThumbUp
-                      size={"13"}
-                      style={{ marginRight: 5 }}
-                      onClick={props.onClickLikeBoard}
-                      id={props.data?.fetchBoard.id}
-                    />{" "}
-                    {props.data?.fetchBoard.likes}
-                  </S.Like>
-                  {/* 댓글 수 나옴 */}
-                  <S.CommentCount>
-                    <MdQuestionAnswer size={"13"} style={{ marginRight: 5 }} />
-                    {props.data?.fetchBoard.commentsCount}
-                  </S.CommentCount>
-                </S.LikeAndCommentCount>
-              </S.LikeAndCommentCountBox>
-            </S.ContentsBox>
+              <S.ContentsBox>
+                <S.Contents>{props.data?.fetchBoard.content}</S.Contents>
+                {/* 번역API 버튼 자리? */}
+                <S.ContentsTranslateBox>
+                  <S.ContentsTranslate>번역한 내용</S.ContentsTranslate>
+                </S.ContentsTranslateBox>
+
+                {/* 댓글 부분 다시 수정할 것 */}
+                <S.LikeAndCommentCountBox>
+                  <S.LikeAndCommentCount>
+                    <S.Like>
+                      <MdThumbUp
+                        size={"13"}
+                        style={{ marginRight: 5 }}
+                        onClick={props.onClickLikeBoard}
+                        id={props.data?.fetchBoard.id}
+                      />{" "}
+                      {props.data?.fetchBoard.likes}
+                    </S.Like>
+                    {/* 댓글 수 나옴 */}
+                    <S.CommentCount>
+                      <MdQuestionAnswer
+                        size={"13"}
+                        style={{ marginRight: 5 }}
+                      />
+                      {props.data?.fetchBoard.commentsCount}
+                    </S.CommentCount>
+                  </S.LikeAndCommentCount>
+                </S.LikeAndCommentCountBox>
+                <GardenDetailCommentList
+                  boardElId={props.boardElId}
+                  loginInfo={loginInfo}
+                />
+                {/* <GardenCommentList boardElId={props.boardElId} /> */}
+              </S.ContentsBox>
+            </S.RightWrapper>
           </S.GardenListBox>
         </S.Wrapper>
       </S.GardenWrapper>
--- f6b2-client/src/components/units/gardenDetail/GardenDetail.styles.ts
+++ f6b2-client/src/components/units/gardenDetail/GardenDetail.styles.ts
@@ -1,91 +1,82 @@
 import styled from "@emotion/styled";
 import { BiEdit } from "react-icons/bi";
 
-export const TmpDiv = styled.div`
-  width: 100%;
-  height: 60px;
-`;
-
 export const GardenWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  height: 100vh;
-`;
-
-export const WrapperLeft = styled.div`
-  width: 285px;
-  height: auto;
-  display: flex;
-  flex-direction: column;
   align-items: center;
-  padding: 98px 47px 0px 0px;
-  position: sticky;
-  top: 60px;
+  width: 100%;
+  height: 730px;
 `;
 
 export const Wrapper = styled.div`
-  width: 510px;
+  width: 850px;
+  height: 600px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   background-color: white;
-  padding-top: 45px;
-`;
-export const WrapperRight = styled.div`
-  width: 285px;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 98px 0px 0px 47px;
-  position: sticky;
-  top: 60px;
+  border-radius: 25px;
+  border: 2px solid #727da1;
 `;
 
 export const GardenListBox = styled.main`
+  display: flex;
+  justify-content: space-between;
   width: 100%;
-  padding: 30px;
-  border-radius: 25px;
-  border: 2px solid #727da1;
+  padding: 20px;
   background-color: white;
-  margin-bottom: 20px;
 `;
 
-export const WriterInfoBox = styled.section`
+export const ContentsImg = styled.img`
+  /* 이미지 연결 후 수정하기 */
+  width: 480px;
+  height: 480px;
+  object-fit: cover;
+  /* 완성 후 삭제 할 것 */
+  background-color: #ffb950;
+`;
+
+export const RightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+`;
+
+export const WriterInfoBox = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: center;
 `;
 
-export const WriterProfile = styled.img`
-  width: 30px;
-  height: 30px;
-  border-radius: 50px;
-  margin-right: 10px;
+export const WriterInfo = styled.div`
+  width: 100%;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+`;
 
-  // test
+export const WriterProfile = styled.img`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+
+  /* 완성 후 삭제 */
   background-color: gray;
 `;
 
-export const WriterInfo = styled.article`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-export const WriterName = styled.strong`
+export const WriterName = styled.div`
   font-weight: 700;
   font-size: 14px;
-  margin-right: 5px;
+  margin: 0px 5px;
 `;
 
 export const CreatedAt = styled.time`
-  margin-top: 1px;
-  font-weight: 400;
-  font-size: 11px;
+  font-size: 8px;
   color: #65676b;
 `;
 
@@ -108,14 +99,6 @@ export const ContentsTranslateBox = styled.article`
   border-top: 1px solid gray;
   border-bottom: 1px solid gray;
   padding: 5px;
-`;
-
-export const ContentsImg = styled.img`
-  width: 335px;
-  height: 221px;
-  margin-top: 30px;
-  // test
-  background-color: gray;
 `;
 
 export const ContentsTranslate = styled.div`
@@ -162,14 +145,6 @@ export const CommentListBtn = styled.button`
 export const LikeAndCommentCount = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
-`;
-
-export const NameRow = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   align-items: center;
 `;
 
