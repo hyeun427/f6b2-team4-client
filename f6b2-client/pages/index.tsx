ae1f7f2ba9c55060c2bb4a47c7e61e427f5aaedc
hyeun427 <hyeun427@gmail.com>
2022-05-30 12:32:22 +0900

    feature-#196

--- f6b2-client/pages/index.tsx
+++ f6b2-client/pages/index.tsx
@@ -4,6 +4,42 @@ export default function Home() {
   return (
     <>
       <Landing1 />
+      <Landing1 />
     </>
   );
 }
+
+// import styled from "@emotion/styled";
+
+// const Wrapper = styled.div`
+//   /* position: relative; */
+//   z-index: 1;
+//   width: 100vw;
+//   height: 100vh;
+// `;
+
+// const Video = styled.video`
+//   /* position: relative; */
+//   z-index: 2;
+//   width: max-content;
+//   width: 100%;
+//   height: 100%;
+//   /* width: 450px; */
+//   object-fit: fill;
+// `;
+
+// const Btn = styled.button`
+//   /* position: absolute; */
+//   z-index: 1000;
+//   width: 100px;
+// `;
+
+// export default function Home() {
+//   return (
+//     <Wrapper>
+//       <Video muted autoPlay loop src="/landing/landing.mp4">
+//       </Video>
+//       <Btn>버튼</Btn>
+//     </Wrapper>
+//   );
+// }
